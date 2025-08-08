8a3c509d92d430276ca6c1472fd5e576d5a80f0b
permalik <permalik@protonmail.com>
2025-08-07 23:32:09 -0500

    feat: impl axum, tokio, serde

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    print!("started blue-core..");
+use axum::{
+	routing::{post},
+	Router,
+	extract::Json,
+};
+use serde::Deserialize;
+
+#[tokio::main]
+async fn main() {
+	let app = Router::new().route("/ingest", post(ingest));
+	let listener = tokio::net::TcpListener::bind("0.0.0.0:9000").await.unwrap();
+
+	axum::serve(listener, app)
+		.await
+		.unwrap();
+}
+
+async fn ingest(Json(payload): Json<Vec<FluentBitLog>>) {
+	for log in payload {
+		println!("Received log: {:?}", log);
+	}
+}
+
+#[derive(Debug, Deserialize)]
+struct FluentBitLog {
+	environment: String,
+	log: String,
+	timestamp: String,
 }
